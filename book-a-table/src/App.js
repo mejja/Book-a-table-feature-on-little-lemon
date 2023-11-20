4bc69968f060a2ad47131fc7c8dee52b1465a4df
mejja <majorkiema.mk@gmail.com>
2023-11-21 00:26:37 +0300

    stled banner area

--- book-a-table/src/App.js
+++ book-a-table/src/App.js
@@ -4,33 +4,46 @@ import Banner from "./components/Banner";
 import DishCard from "./components/DishCard";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import Grid from '@mui/material/Grid';
-import SpacingGrid from './components/SpacingGrid';
 import menus from  "./data.json";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 function App({dishes}) {
   return (
     <div className="App">
     < ResponsiveAppBar />
-    < Banner /> 
-    < SpacingGrid />
+    < Banner />
+    {/* < SpacingGrid /> */}
     <Container>
       {menus.map((menu) => (
         <>
-        <Typography 
-        variant="h4"
-        component="h2"
+        <Stack
+        direction={{xs: "column", sm: "row", md: "row"}}
+        spacing={{xs: 1, sm: 2, md: 4}}
+        marginTop={25}
+        marginBottom={20}>
+        <Typography
+        variant="h1"
+        component="h1"
         marginTop={5}
         marginButtom={3}
         >
-          Top {menu.name} Tours
+          {menu.name}
         </Typography>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        <Button variant="outlined" color="error" size="large">Online Menu</Button>
+        </Stack>
         <Grid container spacing={5}>
           {menu.dishes.map((dish, index) => 
           <DishCard dish={dish} key={index}/>)}
         </Grid>
-        </>
+        </ >
       ))}
-
     </Container>
     </div>
   );
--- book-a-table/src/components/DishCard.js
+++ book-a-table/src/components/DishCard.js
@@ -9,10 +9,12 @@ const DishCard = ({dish}) => {
     return( 
     <Grid item xs={4} marginY={2} sx={{ flexGrow: 1 }} container spacing={2}>
       <Paper 
-      square={false}
-      sx={{borderRadius: 2}}
+      backgroundColor
+      background-color={"rgb(255, 0, 0)"}
+      sx={{borderRadius: 8}}
       elevation={3}>
         <img 
+        style={{borderRadius: 25, height: 300}}
         src={dish.image}
         alt="Menu Dish"
         className="img"
@@ -28,11 +30,21 @@ const DishCard = ({dish}) => {
             display: "flex",
             alignItems: "center"
           }}>
-            <Typography variant="body1" component="span" fontFamily={"Karla, sans-serif"} display={"block"} >
+            <Typography variant="body1" component="span" fontFamily={"Karla, sans-serif"} display={"block"} marginBottom={10} >
             {dish.text}
             </Typography>
         </Box>
+        <Box 
+          paddingX={2}
+          marginBottom={2}
+          sx={{
+          display: "flex",
+          alignItems: "center"
+          }}
+        >
         <Button variant="outlined" color="primary" size="large">{dish.delivery}</Button>
+        </Box>
+        
       </Paper>
     </Grid>
     );
