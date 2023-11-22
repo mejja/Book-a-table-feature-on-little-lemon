91270360d2ec8806dbf4e2ceede830434b6c2907
mejja <majorkiema.mk@gmail.com>
2023-11-23 00:20:39 +0300

    finalized the review section

--- book-a-table/src/components/Testimonials.js
+++ book-a-table/src/components/Testimonials.js
@@ -1,100 +1,111 @@
 import React from "react";
 import "../App.css";
+import Rating from '@mui/material/Rating';
 // card imports
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea} from "@mui/material";
+import { Box, CardActionArea} from "@mui/material";
 
 export default function Testimonials() {
   return (
-    <header style={{ paddingLeft: 0 }}>
-      <div
-        className="mask">
+    <header >
+      <div className="mask">
         <div className="testimonial-header">
           <h1>Testimonials</h1>
         </div>
          <div className="review-container">
             <div className="review">
-              <Card sx={{ maxWidth: 250 }}>
+              <Card sx={{ maxWidth: 250, minHeight: 285}}>
                 <CardActionArea>
-                  <CardMedia
-                  id="profile-img"
-                    component="img"
-                    height="140"
-                    image= {require("../assets/images/Major M.jpg")}
-                    alt="profile picture"
-                  />
+                <Box marginTop={2}>
+                <Rating name="read-only" value={5} readOnly />
+                </Box>
+                <div>
+                <img
+                id="profile-img"
+                src={require("../assets/images/Major_M_75x75.jpg")}
+                alt="Profile"
+                />
+                </div>
                   <CardContent>
-                    <Typography gutterBottom variant="h2" component="div">
+                    <Typography gutterBottom variant="h5" component="div">
                       Major M.
                     </Typography>
                     <Typography id="h2" variant="body2" color="text.secondary">
-                      Really enjoyed their quality food with cooked with African twist.
+                      "Mahali pazuri pa amani na chakula kitamu"
                     </Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
               </div>
               <div className="review">
-              <Card sx={{ maxWidth: 250 }}>
+              <Card sx={{ maxWidth: 250, minHeight: 285 }}>
                 <CardActionArea>
-                  <CardMedia
-                    id="profile-img"
-                    component="img"
-                    height="140"
-                    image= {require("../assets/images/Major M.jpg")}
-                    alt="Profile picture"
-                  />
+                <Box marginTop={2}>
+                <Rating name="read-only" value={5} readOnly />
+                </Box >
+                <div>
+                <img
+                id="profile-img"
+                src={require("../assets/images/Elizabeth_K_75x75.jpg")}
+                alt="Profile"
+                />
+                </div>
                   <CardContent>
-                    <Typography id="h2" gutterBottom variant="h2" component="div">
-                      Major M.
+                    <Typography id="h2" gutterBottom variant="h5" component="div">
+                      Elizabeth K.
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                    Really enjoyed their quality food with cooked with African twist.
+                    "Toller Ort, ruhige Atmosphäre mit leckerem Essen"
                     </Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
               </div>
               < div className="review">
-              <Card sx={{ maxWidth: 250 }}>
+              <Card sx={{ maxWidth: 250, minHeight: 285 }}>
                 <CardActionArea>
-                  <CardMedia
-                    id="profile-img"
-                    component="img"
-                    height="140"
-                    image= {require("../assets/images/Major M.jpg")}
-                    alt="Profile picture"
-                  />
+                <Box marginTop={2}>
+                <Rating name="read-only" value={5} readOnly />
+                </Box>
+                <div>
+                <img
+                id="profile-img"
+                src={require("../assets/images/Joseph_R_75x75.jpg")}
+                alt="Profile"
+                />
+                </div>
                   <CardContent>
-                    <Typography id="h2" gutterBottom variant="h2" component="div">
-                      Major M.
+                    <Typography id="h2" gutterBottom variant="h5" component="div">
+                      Joseph S.
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                    Really enjoyed their quality food with cooked with African twist.
+                    "Super endroit, atmosphère paisible avec une cuisine délicieuse"
                     </Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
               </div>
               <div className="review">
-              <Card sx={{ maxWidth: 250 }}>
-                <CardActionArea>
-                  <CardMedia
-                    id="profile-img"
-                    component="img"
-                    height="140"
-                    image= {require("../assets/images/Major M.jpg")}
-                    alt="Profile picture"
-                  />
-                  <CardContent>
-                    <Typography id="h2" gutterBottom variant="h2" component="div" >
-                      Major M.
+              <Card sx={{ maxWidth: 250, minHeight: 285}}>
+                <CardActionArea >
+                <Box marginTop={2}>
+                <Rating name="read-only" value={5} readOnly />
+                </Box>
+                <div>
+                <img
+                id="profile-img"
+                src={require("../assets/images/Martins_Y._1_75x75.jpg")}
+                alt="Profile"
+                />
+                </div>
+                <CardContent>
+                    <Typography id="h2" gutterBottom variant="h5" component="div" >
+                      Martin Y.
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                    Really enjoyed their quality food with cooked with African twist.
+                    "Really enjoyed their quality food with cooked with African twist."
                     </Typography>
                   </CardContent>
                 </CardActionArea>
