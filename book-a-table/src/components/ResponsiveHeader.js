495046e8220cf1125d831a2b17ecb4e5287e141a
mejja <majorkiema.mk@gmail.com>
2023-12-18 23:13:38 +0300

    removed commented code in ResponsiveHeader.js

--- book-a-table/src/components/ResponsiveHeader.js
+++ book-a-table/src/components/ResponsiveHeader.js
@@ -102,26 +102,6 @@ export default function ResponsiveHeader() {
               ))}
             </Menu>
           </Box>
-          {/* <img src={logo} width="auto" height="auto" alt="logo"/> */}
-          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
-          {/* <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "black",
-              textDecoration: "none",
-            }}
-          >
-            Little Lemon
-          </Typography> */}
           <div className="primary-logo">
            <img src={logo} width="auto" height="auto" alt="Primary logo" />
            </div>
