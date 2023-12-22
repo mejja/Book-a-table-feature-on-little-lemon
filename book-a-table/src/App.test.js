e192942813f4c64d58dd95b91e7b2674a90223ef
mejja <majorkiema.mk@gmail.com>
2023-12-22 23:03:37 +0300

    added first test of reserve a table app

--- book-a-table/src/App.test.js
+++ book-a-table/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReserveForm from './components/ReserveForm';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<ReserveForm />);
+  const linkElement = screen.getByText(/Customer's Name/i);
   expect(linkElement).toBeInTheDocument();
 });
