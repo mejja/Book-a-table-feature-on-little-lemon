b7d9b0922883bb99e31315751c37d705c4551e73
mejja <majorkiema.mk@gmail.com>
2023-12-23 15:43:13 +0300

    grouped my for form tests

--- book-a-table/src/App.test.js
+++ book-a-table/src/App.test.js
@@ -2,8 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import ReserveForm from './components/ReserveForm';
 
-test('renders learn react link', () => {
-  render(<ReserveForm />);
-  const linkElement = screen.getByText(/Customer's Name/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Testing reserve table Form", ()=> {
+  test('renders learn react link', () => {
+    render(<ReserveForm />);
+    const linkElement = screen.getByText(/Customer's Name/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+
+})
+
