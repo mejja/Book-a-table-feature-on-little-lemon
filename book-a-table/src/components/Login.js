12757cb852f7a89b3bc17bc8ad012091b22e8f68
mejja <majorkiema.mk@gmail.com>
2024-01-27 23:03:48 +0300

    switched to card component from paper

--- book-a-table/src/components/Login.js
+++ book-a-table/src/components/Login.js
@@ -1,9 +1,34 @@
-import "../App.css";
-import React from 'react';
-import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
-export default function Login() {
+const card = (
+  <React.Fragment>
+    <CardContent>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
   return (
-  )
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
 }
