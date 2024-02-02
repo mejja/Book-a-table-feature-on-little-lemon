850afabb9da17851b4ec9dfe02c940eb649ae28b
mejja <majorkiema.mk@gmail.com>
2024-02-02 23:38:01 +0300

    refactored about code

--- book-a-table/src/components/About.js
+++ book-a-table/src/components/About.js
@@ -1,5 +1,8 @@
-import * as React from 'react';
-import "../App.css";
-import { Box } from '@mui/material';
+import React from 'react'
 
-export default
+export default function About() {
+  return (
+    <div>About</div>
+  )
+}
+
