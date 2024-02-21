b5d14e8cd28fe61e5d006ae152584f19020e86c0
mejja <majorkiema.mk@gmail.com>
2024-02-21 23:06:58 +0300

    fixed typo

--- book-a-table/src/components/Login.js
+++ book-a-table/src/components/Login.js
@@ -18,7 +18,7 @@ export default function OutlinedCard() {
   return (
     <Box sx={{ minWidth: 275 }}>
       <Card variant="outlined">{card}</Card>
-      <h1>Login</h1>
+      <h1>Login Form</h1>
     </Box>
   );
 }
