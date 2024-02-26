f44870ed4a32c1f45d4c4f4e24644b12dd707452
mejja <majorkiema.mk@gmail.com>
2024-02-26 22:33:28 +0300

    fixed about section

--- book-a-table/src/components/About.js
+++ book-a-table/src/components/About.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import * as React from 'react';
+import "../App.css";
+import { Box } from '@mui/material';
+
+
+
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <Box sx={{borderBottom:7, marginRight:0}}>
+            <div className='about'>
+            <div className='about-content'>
+            <h4>
+              Little Lemon
+            </h4>
+            <h5 >Chicago</h5>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at ex leo. Maecenas enim sem, laoreet at nulla ac, luctus scelerisque massa. Praesent ut molestie nisi. Aliquam arcu lorem, auctor condimentum blandit id, lobortis in nisi. Ut diam justo, euismod in accumsan id, vehicula sit amet tellus. Pellentesque porttitor elit lacus, vitae lacinia magna ultricies quis. Etiam vitae tellus et mi hendrerit consequat. In dictum ligula in elit euismod malesuada. Curabitur varius augue id tempus ultricies. Mauris suscipit porta odio, et pretium nibh scelerisque ut.
+            </p>
+            </div>
+            <div className='owners'>
+            <img id='chefs'src={require('../assets/images/owners.jpg')} alt='Male Owners'/>
+            <img id='restaurant' src={require('../assets/images/restaurant.jpg')} alt='Female Owners' />
+            </div>
+            </div>
+            </Box>
+  );
 }
-
