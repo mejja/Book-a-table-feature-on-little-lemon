4a925755664695d6034dea03d982d52d92e60eec
mejja <majorkiema.mk@gmail.com>
2024-03-31 16:00:02 +0300

    replaced holder text with restaurant description in about section

--- book-a-table/src/components/About.js
+++ book-a-table/src/components/About.js
@@ -13,7 +13,7 @@ export default function About() {
             </h4>
             <h5 >Chicago</h5>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at ex leo. Maecenas enim sem, laoreet at nulla ac, luctus scelerisque massa. Praesent ut molestie nisi. Aliquam arcu lorem, auctor condimentum blandit id, lobortis in nisi. Ut diam justo, euismod in accumsan id, vehicula sit amet tellus. Pellentesque porttitor elit lacus, vitae lacinia magna ultricies quis. Etiam vitae tellus et mi hendrerit consequat. In dictum ligula in elit euismod malesuada. Curabitur varius augue id tempus ultricies. Mauris suscipit porta odio, et pretium nibh scelerisque ut.
+            Little is mediterenian restaurant serverving local cuisine
             </p>
             </div>
             <div className='owners'>
